f5f97dceefe9e4ffae4e465b4a055a98d1e5834f
caspian boiler <aliyabbjni@gmail.com>
2026-09-15 03:55:28 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,47 @@
-const CACHE_NAME = 'caspian-serial-v6';
+const CACHE_NAME = 'caspian-serial-v8';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => Promise.all(
-    ASSETS.map(url => cache.add(url).catch(err => console.warn('cache miss:', url)))
-  )));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => Promise.all(
+      ASSETS.map(url => cache.add(url).catch(err => console.warn('cache miss:', url)))
+    ))
+  );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(
-    keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-  )));
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   const url = event.request.url;
+
   if (url.indexOf('script.google.com') !== -1) {
     event.respondWith(fetch(event.request));
     return;
   }
+
   if (event.request.destination === 'document' || url.endsWith('/index.html') || url.endsWith('/')) {
-    event.respondWith(fetch(event.request).then(response => {
-      const clone = response.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-      return response;
-    }).catch(() => caches.match('./index.html')));
+    event.respondWith(
+      fetch(event.request).then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      }).catch(() => caches.match('./index.html'))
+    );
     return;
   }
-  event.respondWith(caches.match(event.request).then(cached =>
-    cached || fetch(event.request).catch(() => caches.match('./index.html'))
-  ));
+
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request).catch(() => caches.match('./index.html'))
+    )
+  );
 });
