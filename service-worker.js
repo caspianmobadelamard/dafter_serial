291f35fba558523251e8f646e788301e5d346366
caspian boiler <aliyabbjni@gmail.com>
2026-09-20 07:49:48 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'caspian-serial-v12';
+const CACHE_NAME = 'caspian-serial-v13';
 const ASSETS = [
   './', './index.html', './manifest.json',
   './icon-192.png', './icon-512.png',
